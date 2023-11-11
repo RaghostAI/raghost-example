457e36d2244caa4bfc056eddaa747e4b1ac29daa
Marissa Montgomery <m@m15y.com>
2023-11-11 18:02:52 -0500

    potential fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
+import React from "react";
 import "../styles/globals.css";
-import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: any) {
   return <Component {...pageProps} />;
 }
 
-export default MyApp;
+export default App;
